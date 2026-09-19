962e0a919358047caac718a9e1e22a9eb23ef99c
Srishti Dua <Srishtidua2606@gmail.com>
2026-09-19 17:10:02 +0530

    Refactor video schema for improved clarity and structure

--- src/models/video.models.js
+++ src/models/video.models.js
@@ -1,11 +1,44 @@
-import mongoose , {Schema} from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
 
 const videoSchema = new Schema(
     {
-        videoFile :{
-            
-        }
+        videoFile: {
+            type: String, //cloudinary url
+            required: true,
+        },
+        thumbnail: {
+            type: String, //cloudinary url
+            required: true,
+        },
+        title : {
+            type : String,
+            required : true
+        },
+        description : {
+            type : String,
+            required : true 
+    },
+    views:{
+        type : Number,
+        default : 0
+    },
+    duration: {
+        type : Number,
+        required : true
+    },
+    isPublished:{
+        type: Boolean,
+        default : true
+    },
+    owner:{
+        type : mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required : true
     }
+
+},{
+    timestamps:true
+}
 )
 
 export const Video = mongoose.model("Video", videoSchema)
