7a5bcab8e63120a8ef3400caeed6cf7cb9a7d2be
Srishti Dua <Srishtidua2606@gmail.com>
2026-09-21 17:33:10 +0530

    Add cloudinary and multer integration; configure cookie-parser middleware

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 const app = express();
+
 app.use(
     cors({
         origin: process.env.CORS_ORIGIN,
@@ -11,7 +13,7 @@ app.use(
 app.use(express.json({limit:"16kb"}));
 app.use(express.urlencoded({limit:"16kb", extended:true}));
 app.use(express.static("public"));
-
+app.use(cookieParser())
 //IMPORT ROUTES
 import healthCheckRouter from "./routes/healthCheck.routes.js"
 
