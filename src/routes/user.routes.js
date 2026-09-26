ab585ff5fcf5c49e98acefa77cedce70c3c1163f
Srishti Dua <Srishtidua2606@gmail.com>
2026-09-26 17:03:15 +0530

    Add user authentication and profile management routes; enhance user account functionalities

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,18 @@
 import {Router} from "express";
 import { upload } from "../middlewares/multer.middlewares.js";
-import { registerUser , logoutUser } from "../controllers/user.controllers.js";
+import {
+     registerUser ,
+      logoutUser,
+       loginUser, 
+     refreshAccessToken, 
+     changeCurrentPassword,
+     getCurrentUser,
+     getUserChannelProfile,
+     updateAccountDetails,
+     updateUserAvatar,
+     updateUserCoverImg,
+     getWatchHistory
+    } from "../controllers/user.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
 const router = Router();
@@ -15,7 +27,17 @@ router.route('/register').post(
             maxCount:1
         }
     ]),registerUser)
+router.route("/login").post(loginUser)
+router.route("/refresh-token").post(refreshAccessToken)
 
 //secured routes
 router.route('/logout').post(verifyJWT, logoutUser)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").post(verifyJWT, getCurrentUser)
+router.route("/c/:username").post(verifyJWT, getUserChannelProfile)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/update-cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImg)
+router.route("/history").get(verifyJWT, getWatchHistory)
+
 export default router
